3765ae2636de37039035fea005ba4bb49b40eed9
Zotikov Artur <azotikos@yandex.ru>
2026-05-25 19:28:42 +0300

    lesson-3: added validation in simplestore

--- src/Lesson-3/SimpleStore.cs
+++ src/Lesson-3/SimpleStore.cs
@@ -4,9 +4,24 @@ public sealed class SimpleStore
 {
     private readonly Dictionary<string, byte[]> _store = new();
 
-    public void Set(string key, byte[] value) => _store[key] = value;
+    public void Set(string key, byte[] value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(key);
 
-    public byte[]? Get(string key) => _store.GetValueOrDefault(key);
+        _store[key] = value;
+    }
 
-    public void Delete(string key) => _store.Remove(key);
+    public byte[]? Get(string key)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(key);
+
+        return _store.GetValueOrDefault(key);
+    }
+
+    public void Delete(string key)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(key);
+
+        _store.Remove(key);
+    }
 }
