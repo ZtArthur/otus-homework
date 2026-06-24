26b502aaff8100935e123bfdc72bba8c233e4386
Zotikov Artur <azotikos@yandex.ru>
2026-06-24 19:15:00 +0300

    lesson-9: добавлен read/write lock и тесты

--- src/CacheService/SimpleStore.cs
+++ src/CacheService/SimpleStore.cs
@@ -1,27 +1,75 @@
 ﻿namespace CacheService;
 
-public sealed class SimpleStore
+public sealed class SimpleStore : IDisposable
 {
     private readonly Dictionary<string, byte[]> _store = new();
+    private readonly ReaderWriterLockSlim _lock = new();
+
+    private long _setCount;
+    private long _getCount;
+    private long _deleteCount;
 
     public void Set(string key, byte[] value)
     {
         ArgumentException.ThrowIfNullOrEmpty(key);
 
-        _store[key] = value;
+        try
+        {
+            _lock.EnterWriteLock();
+
+            _store[key] = value;
+
+            Interlocked.Increment(ref _setCount);
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
     }
 
     public byte[]? Get(string key)
     {
         ArgumentException.ThrowIfNullOrEmpty(key);
 
-        return _store.GetValueOrDefault(key);
+        try
+        {
+            _lock.EnterReadLock();
+
+            var value = _store.GetValueOrDefault(key);
+
+            Interlocked.Increment(ref _getCount);
+
+            return value;
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
     }
 
     public void Delete(string key)
     {
         ArgumentException.ThrowIfNullOrEmpty(key);
 
-        _store.Remove(key);
+        try
+        {
+            _lock.EnterWriteLock();
+
+            _store.Remove(key);
+
+            Interlocked.Increment(ref _deleteCount);
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+
+    public (long setCount, long getCount, long deleteCount) GetStatistics() => (_setCount, _getCount, _deleteCount);
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        _lock.Dispose();
     }
 }
---
+++ tests/CacheService.Tests/SimpleStoreReadWriterLockTests.cs
@@ -0,0 +1,52 @@
+﻿namespace CacheService.Tests;
+
+public class SimpleStoreReadWriterLockTests
+{
+    [Theory]
+    [InlineData(5, 5, 50, 50)]
+    [InlineData(25, 25, 150, 150)]
+    public async Task should_return_correct_read_write_statistics(int readerTasksCount, int writerTasksCount, int readCount, int writeCount)
+    {
+        using var store = new SimpleStore();
+        var expectedReadCount = readerTasksCount * readCount;
+        var expectedWriteCount = writerTasksCount * writeCount;
+        var key = "item";
+
+        var readerTasks = Enumerable.Range(start: 0, readerTasksCount)
+            .Select(_ =>
+                {
+                    return Task.Run(() =>
+                        {
+                            for (var i = 0; i < readCount; i++)
+                            {
+                                store.Get(key);
+                            }
+                        }
+                    );
+                }
+            )
+            .ToArray();
+
+        var writerTasks = Enumerable.Range(start: 0, writerTasksCount)
+            .Select(_ =>
+                {
+                    return Task.Run(() =>
+                        {
+                            for (var i = 0; i < writeCount; i++)
+                            {
+                                store.Set(key, [byte.MinValue]);
+                            }
+                        }
+                    );
+                }
+            )
+            .ToArray();
+
+        await Task.WhenAll([..readerTasks, ..writerTasks]);
+
+        var statistics = store.GetStatistics();
+
+        Assert.Equal(expectedReadCount, statistics.getCount);
+        Assert.Equal(expectedWriteCount, statistics.setCount);
+    }
+}
