678f2febb63c19468c35842eba557b3d979de40d
Zotikov Artur <azotikos@yandex.ru>
2026-08-28 17:37:51 +0300

    lesson-21: нагрузочный тест

---
+++ benchmarks/CacheService.Benchmarks/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using CacheService.Benchmarks;
+using CacheService.Models;
+
+BenchmarkRunner.Run<SerializationBenchmarks>();
+
+namespace CacheService.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class SerializationBenchmarks
+    {
+        private UserProfile _profile = null!;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _profile = new UserProfile
+            {
+                Id = 1031,
+                CreatedAt = DateTime.Now,
+                Username = "user-benchmark"
+            };
+        }
+
+        [Benchmark(Baseline = true)]
+        public byte[] SystemTextJson()
+        {
+            using var ms = new MemoryStream();
+            JsonSerializer.Serialize(ms, _profile);
+
+            return ms.ToArray();
+        }
+
+        [Benchmark]
+        public byte[] GenerateBinary()
+        {
+            using var ms = new MemoryStream();
+            _profile.SerializeToBinary(ms);
+
+            return ms.ToArray();
+        }
+    }
+}
